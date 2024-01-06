5cc1263b325a2ab51831807e399b1a85cec040b7
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-01-06 12:49:22 -0300

    Add project files.

---
+++ code verifier and code challenge generator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string codeVerifier = GenerateVerifier();
+
+Console.WriteLine($"Generated Code Verifier: {codeVerifier}");
+Console.WriteLine("BASE64URL-ENCODE(SHA256(ASCII(code_verifier))): " + Convert.ToBase64String(SHA256.HashData(Encoding.ASCII.GetBytes(codeVerifier)))
+    .TrimEnd('=')
+    .Replace('+', '-')
+    .Replace('/', '_'));
+Console.Read();
+
+static string GenerateVerifier()
+{
+    byte[] buffer = new byte[32];
+    using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+    {
+        rng.GetBytes(buffer);
+    }
+    string base64 = Convert.ToBase64String(buffer).Replace('+', '-').Replace('/', '_').TrimEnd('=');
+
+    return base64[..Math.Max(43, Math.Min(128, base64.Length))];
+}
